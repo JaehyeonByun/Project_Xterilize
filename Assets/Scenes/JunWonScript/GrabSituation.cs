179c02c86c5a2d3a96095c78adc2df76924bdaf3
ohjunwon <hwn76712@naver.com>
2024-06-16 17:03:46 +0900

    Add LockController pos

--- Assets/Scenes/JunWonScript/GrabSituation.cs
+++ Assets/Scenes/JunWonScript/GrabSituation.cs
@@ -5,18 +5,19 @@ using UnityEngine.XR;
 
 public class GrabSituation : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] bool isLeftDetected;
     [SerializeField] bool isRightDetected;
 
-    [SerializeField] InputDevice targetDevice;
     [SerializeField] bool isLeftGripPressed;
     [SerializeField] bool isRightGripPressed;
 
     [SerializeField] bool isStartSimulation;
 
+    [SerializeField] Transform leftHandTransform;
+    [SerializeField] Transform rightHandTransform;
 
-
+    private Vector3 leftHandPreviousPosition;
+    private Vector3 rightHandPreviousPosition;
 
     void Start()
     {
@@ -26,60 +27,55 @@ public class GrabSituation : MonoBehaviour
         isLeftGripPressed = false;
         isRightGripPressed = false;
 
-        // 넘겨받아 고정하고 있는 상황 시작
         isStartSimulation = false;
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // OVRInput 계속 좌우 눌러져 있는 지 확인
-        isLeftGripPressed = 
-            OVRInput.Get(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch);
-        isRightGripPressed =
-            OVRInput.Get(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch);
+        isLeftGripPressed = OVRInput.Get(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        isRightGripPressed = OVRInput.Get(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch);
 
-        //DebugGripPressed();
-        
-        if (isLeftDetected && isRightDetected &&
-            isLeftGripPressed && isRightGripPressed &&
-            !isStartSimulation)
+        if (isLeftDetected && isRightDetected && isLeftGripPressed && isRightGripPressed && !isStartSimulation)
         {
             isStartSimulation = true;
             Debug.Log("Start Simulation");
-            Debug.Log("Don\'t release your hold here");
+            Debug.Log("Don't release your hold here");
+
+            leftHandPreviousPosition = leftHandTransform.position;
+            rightHandPreviousPosition = rightHandTransform.position;
         }
-        else if(isStartSimulation && (!isLeftGripPressed || !isRightGripPressed))
+        else if (isStartSimulation && (!isLeftGripPressed || !isRightGripPressed))
         {
             Debug.Log("End Simulation");
+            isStartSimulation = false;
         }
 
-        
-           
+        if (isStartSimulation)
+        {
+            LockControllerPosition();
+        }
     }
-
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "LeftHand") { 
+        if (other.CompareTag("LeftHand"))
+        {
             isLeftDetected = true;
-            Debug.Log(other.name + "detected");
+            Debug.Log(other.name + " detected");
         }
-        if (other.tag == "RightHand") {
+        if (other.CompareTag("RightHand"))
+        {
             isRightDetected = true;
-            Debug.Log(other.name + "detected");
-        };
+            Debug.Log(other.name + " detected");
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "LeftHand") isLeftDetected = false;
-        if (other.tag == "RightHand") isRightDetected = false;
+        if (other.CompareTag("LeftHand")) isLeftDetected = false;
+        if (other.CompareTag("RightHand")) isRightDetected = false;
     }
 
-
-    // Debug Oculus gripButton bool value 
     private void DebugGripPressed()
     {
         if (isLeftGripPressed)
@@ -101,5 +97,14 @@ public class GrabSituation : MonoBehaviour
         }
     }
 
+    void LockControllerPosition()
+    {
+        // 그립 버튼이 눌려있을 때 컨트롤러 위치를 고정
+        OVRInput.SetControllerVibration(0.5f, 0.5f, OVRInput.Controller.LTouch); // 진동 추가 (선택사항)
+        OVRInput.SetControllerVibration(0.5f, 0.5f, OVRInput.Controller.RTouch); // 진동 추가 (선택사항)
 
+        // 여기서는 컨트롤러의 Transform을 이전 위치로 설정하여 고정시킵니다.
+        leftHandTransform.position = leftHandPreviousPosition;
+        rightHandTransform.position = rightHandPreviousPosition;
+    }
 }
