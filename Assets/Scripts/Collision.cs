98d78987c12b6502aa63cb93620b8e533b77ef20
Seyoung <seyoung@30Mackbookair.local>
2024-06-25 17:55:28 +0900

    #15 [feature] Conta 효과 추가

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,35 +4,121 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 using static Unity.Burst.Intrinsics.X86.Avx;
 using System.Text;
 
 public class Collision : MonoBehaviour
 {
-    
+
+    public Material Contamination;
+    public GameObject arrow_down; 
+    private List<GameObject> contaminatedObjects = new List<GameObject>();
+
+    private Canvas hitEffectCanvas;
+    private Image hitEffectImage;
+
+    private void Start()
+    {
+        // load arrow
+        arrow_down = Resources.Load<GameObject>("Prefabs/Arrow_down");
+
+        // hit effect Canvas
+        GameObject canvasGameObject = new GameObject("HitEffectCanvas");
+        hitEffectCanvas = canvasGameObject.AddComponent<Canvas>();
+        hitEffectCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        hitEffectCanvas.sortingOrder = 100;
+
+        // hit effect Image
+        GameObject imageGameObject = new GameObject("HitEffectImage");
+        imageGameObject.transform.SetParent(canvasGameObject.transform);
+        hitEffectImage = imageGameObject.AddComponent<Image>();
+        hitEffectImage.color = new Color(1, 0, 0, 0.3f); 
+        hitEffectImage.rectTransform.anchorMin = new Vector2(0, 0);
+        hitEffectImage.rectTransform.anchorMax = new Vector2(1, 1);
+        hitEffectImage.rectTransform.offsetMin = Vector2.zero;
+        hitEffectImage.rectTransform.offsetMax = Vector2.zero;
+
+        hitEffectCanvas.enabled = false;
+    }
 
     private void OnCollisionEnter(UnityEngine.Collision collision)
-    {
-        if (collision.gameObject.GetComponent<Collider>().gameObject.CompareTag("Player"))
+    { 
+        GameObject collidedObject = collision.gameObject;
+
+        string currentTime = System.DateTime.Now.ToString("HHï¿½ï¿½mmï¿½ï¿½ssï¿½ï¿½");
+        Debug.Log("Current Time: " + currentTime);
+        GameManager.timeLog.Add(currentTime);
+        GameManager.ContaminationLog.Add("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½Ã¼ ï¿½ï¿½ï¿½ï¿½");
+        GameManager.WhyLog.Add(this.gameObject.name);
+
+        if (collidedObject.CompareTag("NotConta"))
         {
-            //Debug.Log("Player Contaminated!");
-            
-            string currentTime = System.DateTime.Now.ToString("HH½ÃmmºÐssÃÊ");
-            Debug.Log("Current Time: " + currentTime);
-            GameManager.timeLog.Add(currentTime);
-            GameManager.ContaminationLog.Add("¿À¿°µÈ ¹°Ã¼ Á¢ÃË");
-            GameManager.WhyLog.Add(this.gameObject.name);
+            //Debug.Log("Collision With NotConta Object!");
+        }
+        else
+        {
+            if (this.gameObject.CompareTag("Hand"))
+            {
+                collidedObject.tag = "HandConta";
 
+                AddOverlay addOverlay = collidedObject.AddComponent<AddOverlay>();
+                addOverlay.overlayMaterial = Contamination;
+                addOverlay.ApplyOverlay();
 
+                contaminatedObjects.Add(collidedObject);
+
+                StartCoroutine(BlinkColor(collidedObject, Contamination, 1f, 3));
+            }
+
+            else if (this.gameObject.CompareTag("Body"))
+            {
+                collidedObject.tag = "BodyConta";
+
+                contaminatedObjects.Add(collidedObject);
+
+                StartCoroutine(ShowHitEffect(1f));
+            }
         }
     }
+
+    private IEnumerator BlinkColor(GameObject obj, Material contaminationMaterial, float blinkDuration, int blinkCount)
+    {
+        Renderer renderer = obj.GetComponent<Renderer>();
+        Material originalMaterial = renderer.material;
+
+        for (int i = 0; i < blinkCount; i++)
+        {
+            renderer.material = contaminationMaterial;
+            yield return new WaitForSeconds(blinkDuration);
+            renderer.material = originalMaterial;
+            yield return new WaitForSeconds(blinkDuration);
+        }
+    }
+
+    private IEnumerator ShowHitEffect(float duration)
+    {
+        hitEffectCanvas.enabled = true;
+
+        yield return new WaitForSeconds(duration);
+
+        hitEffectCanvas.enabled = false;
+    }
+
     void Update()
     {
         //if (Input.GetKeyDown(KeyCode.T))
         //{
         //    MakeCsv(); 
         //}
+
+        // When press the Feedback button, call function
+        //if (Input.GetKeyDown(KeyCode.))
+        //{
+        //    ShowArrowsOnContaminatedObjects();
+        //}
     }
+
     void MakeCsv()
     {
 
@@ -65,8 +151,16 @@ public class Collision : MonoBehaviour
         {
             Debug.Log("An error occurred while writing to the CSV file: " + ex.Message);
         }
+    }
 
-
+    // If press the feedback button, 
+    void ShowArrowsOnContaminatedObjects()
+    {
+        foreach (GameObject obj in contaminatedObjects)
+        {
+            // Attach arrow to objects of Contalist
+            Instantiate(arrow_down, obj.transform.position + Vector3.up * 2, Quaternion.identity);
+        }
     }
 }
-    
+
